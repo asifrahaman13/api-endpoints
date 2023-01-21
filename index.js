5496f286c839380ba5e17707d288092e14d8da22
asifrahaman13 <asifrahaman162@gmail.com>
2023-01-21 22:25:09 +0530

    another commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app = express();
 var cors=require('cors');
 
-const port=5000;
+const port=process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
